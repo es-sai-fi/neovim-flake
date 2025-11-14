4b3c03fc053f5a56056f153bc48104deb75300ea
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-11-14 13:50:40 -0500

    hopefully it works now?

---
+++ config.nix
@@ -0,0 +1,49 @@
+{ pkgs, ... }:
+{
+  initLua = ''
+    require('config')
+    require('lz.n').load('lazy')
+  '';
+
+  plugins.dev.es-sai-fi = {
+    pure = lib.fileset.toSource {
+      root = ./.;
+      fileset = lib.fileset.unions [
+        ./config
+        ./lazy
+      ];
+    };
+  };
+
+  plugins = {
+    start = [
+      pkgs.vimPlugins.lz-n
+      pkgs.vimPlugins.tokyonight-nvim
+      pkgs.vimPlugins.snacks-nvim
+      (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
+        p.nix
+        p.python
+        p.typescript
+        p.javascript
+        p.rust
+        p.go
+        p.c
+        p.cpp
+        p.lua
+      ]))
+    ];
+
+    opt = [
+      pkgs.vimPlugins.gitsigns-nvim
+      pkgs.vimPlugins.oil-nvim
+      pkgs.vimPlugins.conform-nvim
+      pkgs.vimPlugins.nvim-lint
+      pkgs.vimPlugins.flash-nvim
+      pkgs.vimPlugins.yazi-nvim
+      pkgs.vimPlugins.which-key-nvim
+      pkgs.vimPlugins.mini-surround
+      pkgs.vimPlugins.mini-pairs
+      pkgs.vimPlugins.nvim-lspconfig
+    ];
+  };
+}
--- flake.nix
+++ flake.nix
@@ -26,34 +26,12 @@
       };
     in
     {
-      packages.${system}.default = mnw.lib.wrap pkgs {
-        inherit (pkgs) neovim;
+      packages.${system} = {
+        default = mnw.lib.wrap pkgs.neovim ./config.nix;
+      };
 
-        initLua = ''
-          require('config')
-          require('lz.n').load('lazy')
-        '';
-
-        plugins = {
-          start = [
-            pkgs.vimPlugins.lz-n
-            pkgs.vimPlugins.tokyonight-nvim
-            pkgs.vimPlugins.snacks-nvim
-          ];
-
-          opt = [
-            pkgs.vimPlugins.gitsigns-nvim
-            pkgs.vimPlugins.oil-nvim
-            pkgs.vimPlugins.conform-nvim
-            pkgs.vimPlugins.nvim-lint
-            pkgs.vimPlugins.flash-nvim
-            pkgs.vimPlugins.yazi-nvim
-            pkgs.vimPlugins.which-key-nvim
-            pkgs.vimPlugins.mini-surround
-            pkgs.vimPlugins.mini-pairs
-            pkgs.vimPlugins.nvim-lspconfig
-          ];
-        };
+      homeManagerModules = {
+        default = mnw.homeManagerModules.mnw;
       };
     };
 }
