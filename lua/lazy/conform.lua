064756551658ccdcd5ae43a8defe7105dabeef84
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-11-14 16:22:32 -0500

    fix: properly configured conform and nvim-lint?

--- lua/lazy/conform.lua
+++ lua/lazy/conform.lua
@@ -1,23 +1,27 @@
 return {
 	"conform.nvim",
 	event = { "BufWritePre" },
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			go = { "gofmt" },
-			rust = { "rustfmt" },
-			python = { "ruff_format" },
-			javascript = { "biome" },
-			typescript = { "biome" },
-			json = { "biome" },
-			jsonc = { "biome" },
-			html = { "biome" },
-			css = { "biome" },
-			nix = { "alejandra" },
-		},
-		format_on_save = {
-			timeout_ms = 2000,
-			lsp_format = "fallback",
-		},
-	},
+	config = function()
+    local conform = require("conform")
+
+		conform.setup({
+      formatters_by_ft = {
+			  lua = { "stylua" },
+			  go = { "gofmt" },
+			  rust = { "rustfmt" },
+			  python = { "ruff_format" },
+			  javascript = { "biome" },
+			  typescript = { "biome" },
+			  json = { "biome" },
+			  jsonc = { "biome" },
+			  html = { "biome" },
+			  css = { "biome" },
+			  nix = { "alejandra" },
+		  },
+		  format_on_save = {
+			  timeout_ms = 2000,
+			  lsp_format = "fallback",
+		  },
+    })
+  end,
 }
--- lua/lazy/lint.lua
+++ lua/lazy/lint.lua
@@ -1,14 +1,20 @@
 return {
 	"nvim-lint",
-	lua = { "luacheck" },
-	go = { "golangcilint" },
-	rust = { "clippy" },
-	python = { "ruff" },
-	javascript = { "biome" },
-	typescript = { "biome" },
-	json = { "biome" },
-	jsonc = { "biome" },
-	html = { "biome" },
-	css = { "biome" },
-	nix = { "statix" },
+	config = function({
+    local lint = require("nvim-lint")
+
+    lint.linters_by_ft = {
+      lua = { "luacheck" },
+      go = { "golangcilint" },
+      rust = { "clippy" },
+      python = { "ruff" },
+      javascript = { "biome" },
+      typescript = { "biome" },
+      json = { "biome" },
+      jsonc = { "biome" },
+      html = { "biome" },
+      css = { "biome" },
+      nix = { "statix" },
+    } 
+  end,
 }
