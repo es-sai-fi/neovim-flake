2c7d820b3329b91595e42c7f61c0339fa5543c0b
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-11-14 19:18:54 -0500

    feat: snacks.lua changes

--- lua/lazy/snacks.lua
+++ lua/lazy/snacks.lua
@@ -575,7 +575,6 @@ return {
 	},
 	init = function()
 		vim.api.nvim_create_autocmd("User", {
-			pattern = "VeryLazy",
 			callback = function()
 				-- Create some toggle mappings
 				Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
