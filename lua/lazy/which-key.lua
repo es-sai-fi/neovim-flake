a1d3ccb7c68a29cc7e634dd8f3bac5dbbb59440a
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-11-14 22:07:11 -0500

    feat: actual correct lz.n config finally?

--- lua/lazy/which-key.lua
+++ lua/lazy/which-key.lua
@@ -12,37 +12,34 @@ return {
 	after = function()
 		local wk = require("which-key")
 
-		wk.setup({
-			preset = "helix",
-			spec = {
-				{
-					mode = { "n", "x" },
-					{ "<leader><tab>", group = "tabs" },
-					{ "<leader>s", group = "grep/search" },
-					{ "<leader>p", group = "picker" },
-					{ "<leader>f", group = "file/find" },
-					{ "<leader>g", group = "git" },
-					{ "<leader>u", group = "ui" },
-					{ "g", group = "goto" },
-					{ "gs", group = "surround" },
-					{
-						"<leader>b",
-						group = "buffer",
-						expand = function()
-							return require("which-key.extras").expand.buf()
-						end,
-					},
-					{
-						"<leader>w",
-						group = "windows",
-						proxy = "<c-w>",
-						expand = function()
-							return require("which-key.extras").expand.win()
-						end,
-					},
-					{ "gx", desc = "Open with system app" },
-				},
+		wk.add({
+			{
+				mode = { "n", "x" },
+				{ "<leader><tab>", group = "tabs" },
+				{ "<leader>s", group = "grep/search" },
+				{ "<leader>p", group = "picker" },
+				{ "<leader>f", group = "file/find" },
+				{ "<leader>g", group = "git" },
+				{ "<leader>u", group = "ui" },
+				{ "g", group = "goto" },
+				{ "gs", group = "surround" },
 			},
+			{
+				"<leader>b",
+				group = "buffer",
+				expand = function()
+					return require("which-key.extras").expand.buf()
+				end,
+			},
+			{
+				"<leader>w",
+				group = "windows",
+				proxy = "<c-w>",
+				expand = function()
+					return require("which-key.extras").expand.win()
+				end,
+			},
+			{ "gx", desc = "Open with system app" },
 		})
 	end,
 }
