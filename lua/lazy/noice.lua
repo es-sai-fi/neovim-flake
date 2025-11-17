6f634a335d0ebd0ae5a0f255a95d3ed3c15a0a77
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-11-17 01:41:29 -0500

    feat: add dependencies to noice.lua

--- lua/lazy/noice.lua
+++ lua/lazy/noice.lua
@@ -1,17 +1,25 @@
 return {
-	"noice.nvim",
-	event = "DeferredUIEnter",
-	before = function()
-		local lzn = require("lz.n")
+	{
+		"nui.nvim",
+	},
+	{
+		"nvim-notify",
+	},
+	{
+		"noice.nvim",
+		event = "DeferredUIEnter",
+		before = function()
+			local lzn = require("lz.n")
 
-		lzn.trigger_load("nui.nvim")
-		lzn.trigger_load("nvim-notify")
-	end,
-	after = function()
-		require("noice").setup({
-			presets = {
-				command_palette = false,
-			},
-		})
-	end,
+			lzn.trigger_load("nui.nvim")
+			lzn.trigger_load("nvim-notify")
+		end,
+		after = function()
+			require("noice").setup({
+				presets = {
+					command_palette = false,
+				},
+			})
+		end,
+	},
 }
