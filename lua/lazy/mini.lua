03eb3af1e068e24aa6aa32300bd095169a39b6a6
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-11-17 14:35:40 -0500

    feat: mini.pairs defaults and enable it on terminal and commands

--- lua/lazy/mini.lua
+++ lua/lazy/mini.lua
@@ -3,10 +3,41 @@ return {
 		"mini.pairs",
 		event = { "BufWritePre", "BufNewFile" },
 		after = function()
-			require("mini.pairs").setup({})
+			require("mini.pairs").setup({
+				{
+					modes = { insert = true, command = true, terminal = true },
+					mappings = {
+						["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+						["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+						["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
+
+						[")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+						["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+						["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+
+						['"'] = {
+							action = "closeopen",
+							pair = '""',
+							neigh_pattern = "[^\\].",
+							register = { cr = false },
+						},
+						["'"] = {
+							action = "closeopen",
+							pair = "''",
+							neigh_pattern = "[^%a\\].",
+							register = { cr = false },
+						},
+						["`"] = {
+							action = "closeopen",
+							pair = "``",
+							neigh_pattern = "[^\\].",
+							register = { cr = false },
+						},
+					},
+				},
+			})
 		end,
 	},
-
 	{
 		"mini.surround",
 		after = function()
