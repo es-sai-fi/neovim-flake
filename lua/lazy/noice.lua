77e72ad8de301574799111c02f65e2d4080ff5ec
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-11-17 01:43:04 -0500

    revert previous commit

--- lua/lazy/noice.lua
+++ lua/lazy/noice.lua
@@ -1,25 +1,17 @@
 return {
-	{
-		"nui.nvim",
-	},
-	{
-		"nvim-notify",
-	},
-	{
-		"noice.nvim",
-		event = "DeferredUIEnter",
-		before = function()
-			local lzn = require("lz.n")
+	"noice.nvim",
+	event = "DeferredUIEnter",
+	before = function()
+		local lzn = require("lz.n")
 
-			lzn.trigger_load("nui.nvim")
-			lzn.trigger_load("nvim-notify")
-		end,
-		after = function()
-			require("noice").setup({
-				presets = {
-					command_palette = false,
-				},
-			})
-		end,
-	},
+		lzn.trigger_load("nui.nvim")
+		lzn.trigger_load("nvim-notify")
+	end,
+	after = function()
+		require("noice").setup({
+			presets = {
+				command_palette = false,
+			},
+		})
+	end,
 }
