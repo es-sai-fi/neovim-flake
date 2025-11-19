7543174d8dca76c607d095c9cf1c452f7f614a5a
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-11-18 19:20:02 -0500

    feat: gruvbox material config

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,6 +1,8 @@
 local g = vim.g
 
-g.gruvbox_material_background = "hard"
+g.gruvbox_material_background = "medium"
 g.gruvbox_material_enable_bold = 1
 g.gruvbox_material_enable_italic = 1
+g.gruvbox_material_better_performance = 1
+g.gruvbox_material_float_style = "dim"
 vim.cmd.colorscheme("gruvbox-material")
