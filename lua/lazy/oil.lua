d19683a18fba907520799d0565af61b290e06822
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-11-24 00:08:45 -0500

    oil: skip confirmation, watch for changes

--- lua/lazy/oil.lua
+++ lua/lazy/oil.lua
@@ -72,6 +72,8 @@ return {
 				"mtime",
 			},
 			skip_confirm_for_simple_edits = true,
+			prompt_save_on_select_new_entry = false,
+			watch_for_changes = true,
 			view_options = {
 				show_hidden = false,
 				is_hidden_file = function(name, bufnr)
