86db6fcbe70d104e507733a4441471dd4412d4ba
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-01 01:47:02 -0500

    dummy

--- lua/lazy/snacks.lua
+++ lua/lazy/snacks.lua
@@ -630,8 +630,6 @@ return {
 			scroll = { enabled = true },
 			statuscolumn = { enabled = true },
 			words = { enabled = true },
-			lazygit = { enabled = false },
-			image = { enabled = false },
 		})
 	end,
 }
